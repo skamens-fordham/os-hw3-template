10a5808d1425af90b337692849558a2660fa57e3
Sam Kamens <skamens1@fordham.edu>
2021-08-08 17:52:40 -0400

    Mostly-complete template file

--- bounded_buffer.cpp
+++ bounded_buffer.cpp
@@ -18,6 +18,10 @@ int run_time;
 int *buffer;
 int buffer_size;
 
+// The number of producers and consumers we will have
+int num_producers;
+int num_consumers;
+
 // TODO: Declare the semaphores that you will need to implement this algorithm
 //       Semaphores are of type sem_t
 
@@ -34,69 +38,78 @@ int buffer_size;
 atomic_int producer_num(0);
 atomic_int consumer_num(0);
 
+
+
+// This is the function that will be executed by each producer thread.
 void * producer(void * arg)
 {
-    int myNum = producer_num.fetch_add(1) + 1;
-    int sleep_time;
 
+    // Set my unique thread number, to be used in the output statements below.
+    int myNum = producer_num.fetch_add(1) + 1;
+ 
     while(true) {
 
-        // Sleep for a random amount of time between 1 and 5 seconds
-        sleep_time = (rand() % 5) + 1;
-        sleep(sleep_time);
+        // TODO: Sleep for a random amount of time between 1 and 5 seconds
+        //       Make sure to save the amount of time that was slept, so it can 
+        //       be printed out below.
 
-        // "produce" a random number between 1 and 100
-        int item = (rand() % 100) + 1;
 
-        // See if there's room for me to write
-        sem_wait(&sem_empty);
 
-        // Now that I have room to write, get the lock on the buffer
-        // so I can update it. By waiting to get the lock until
-        // I have passed the semaphore, I can be confident that
-        // I will have a spot to write in, and I hold the lock for
-        // as little time as possible
+        int item;
 
-        pthread_mutex_lock(&buffer_mutex);
+        // TODO: "Produce" an item by generating a random number between 1 and 100 and 
+        //       assigning it to "item"
 
-        buffer[next_write] = item;
-        cout << myNum << ": slept " << sleep_time << ", produced " << item << " in slot " << next_write << endl;
+        
 
-        next_write = (next_write + 1) % buffer_size;
+        // TODO: Implement the meat of the producer algorithm, using any semaphores and
+        //       mutex locks that are needed to ensure safe concurrent access.
+        //
+        //       After producing your item and storing it in the buffer, print a line
+        //       in the following format:
+        //
+        //       <myNum>: slept <seconds>, produced <item> in slot <buffer slot>
+        //
+        //       For example: 
+        //       7: slept 5, produced 27 in slot 2
 
-        sem_post(&sem_full);
-        pthread_mutex_unlock(&buffer_mutex);
     }
+
 }
 
 void * consumer(void * arg)
 {
+    // Set my unique thread number, to be used in the output statements below.
     int myNum = consumer_num.fetch_add(1) + 1;
-    int sleep_time;
     
     while(true) {
-        // Sleep for a random amount of time between 1 and 5 seconds
-        sleep_time = (rand() % 5) + 1;
-        sleep(sleep_time);
 
-        // Wait for there to be something for me to read
-        sem_wait(&sem_full);
+        // TODO: Sleep for a random amount of time between 1 and 5 seconds
+        //       Make sure to save the amount of time that was slept, so it can 
+        //       be printed out below.
 
-        // Now that I have been told there's something to 
-        // read, I'll get the lock on the buffer
-        // so I can read it. By waiting to get the lock until
-        // I have passed the semaphore, I can be confident that
-        // there will be something for me to read and I 
-        // minimize the time spent holding the lock
+        
+        // The item that will be consumed
+        int item;
+        
+        // TODO: "Produce" an item by generating a random number between 1 and 100 and 
+        //       assigning it to "item"
 
-        pthread_mutex_lock(&buffer_mutex);
+        
 
-        int item = buffer[next_read];
-        cout << myNum << ": slept " << sleep_time << ", consumed " << item << " in slot " << next_read << endl;
-        next_read = (next_read + 1) % buffer_size;
+        // TODO: Implement the meat of the consumer algorithm, using any semaphores and
+        //       mutex locks that are needed to ensure safe concurrent access.
+        //
+        //       After consuming your item and storing it in "item", print a line
+        //       in the following format:
+        //
+        //       <myNum>: slept <seconds>, produced <item> in slot <buffer slot>
+        //
+        //       For example: 
+        //       2: slept 3, consumed 22 from slot 2
 
-        sem_post(&sem_empty);
-        pthread_mutex_unlock(&buffer_mutex);
+
+
     }
 }
 
@@ -105,47 +118,25 @@ int main(int argc, char ** argv)
 {
     // Usage: bounded_buffer <run_time> <buffer_size> <num_producers> <num_consumers>
 
-    if (argc != 5) {
-        cerr << "Wrong number of arguments. Usage: bounded_buffer <run_time> <buffer_size> <num_producers> <num_consumers>" << endl;
-        exit(1);
-    }
 
-    run_time = atoi(argv[1]);
-    if (run_time <=0) {
-        cerr << "run_time must be greater than 0" << endl;
-        exit(2);
-    }
+    // TODO: Process the command line arguments. If there are not enough arguments, or if any of the 
+    //       arguments is <= 0, exit with an error message.
+    
 
 
-    buffer_size = atoi(argv[2]);
-     if (buffer_size <=0) {
-        cerr << "buffer_size must be greater than 0" << endl;
-        exit(3);
-    }   
+    // TODO: Allocate your buffer as an array of the correct size based on the command line argument
+    
 
-    int num_producers;
-    num_producers = atoi(argv[3]);
-    if (num_producers <= 0) {
-        cerr << "num_producers must be greater than 0" << endl;
-        exit(4);
-    }
-
-    int num_consumers;
-    num_consumers = atoi(argv[4]);
-    if (num_producers <= 0) {
-        cerr << "num_producers must be greater than 0" << endl;
-        exit(5);
-    }
-
-    buffer = new int[buffer_size];
-
-    // Initialize the semaphores
-    sem_init(&sem_empty, 0, buffer_size);
-    sem_init(&sem_full, 0, 0);
+    // TODO: Initialize the semaphore(s) you are using in your algorithm.
+    //       Use the function sem_init() - see https://man7.org/linux/man-pages/man3/sem_init.3.html
+    //   
 
 
-    // Initialize the random number generator
+    // Initialize the random number generator (see https://man7.org/linux/man-pages/man3/srand.3.html)
     srand(time(NULL));
+
+
+    // Start the producer and consumer threads.
 
     pthread_attr_t attr;
     pthread_attr_init(&attr);
@@ -161,6 +152,12 @@ int main(int argc, char ** argv)
     }
 
 
+    // Now the main program sleeps for as long as the program is supposd to run (based on the command line
+    // argument). While the main thread is sleeping, all of the child threads will be busily producing and consuming.
     sleep(run_time);
+
+    // When the main program is done sleeping, we exit. This will cause all the threads to exit.
+    // In other applications, the main program might want to wait for the child threads to complete. 
+    // This is accomplished with the pthread_join() function (https://man7.org/linux/man-pages/man3/pthread_join.3.html)
     exit(0);
 }
